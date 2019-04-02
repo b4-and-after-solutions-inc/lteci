2aa7f8517d013b5859c815cd097da0c5b07c1605
GMT <gyrome2.itracksolutions@gmail.com>
2019-04-02 10:31:22 +0800

    Edited some Stuffs

--- application/controllers/Access.php
+++ application/controllers/Access.php
@@ -1,10 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Access extends CI_Controller
+{
 
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
+
+	public function login()
+	{
+
+	}
+
 }
