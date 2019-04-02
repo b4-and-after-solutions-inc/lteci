05d22ec9d5b1b48ed9f001eaf43e99dfdcc06c51
GMT <gyrome2.itracksolutions@gmail.com>
2019-04-02 11:28:35 +0800

    Changed Footer Details

--- application/views/includes/cifoot.php
+++ application/views/includes/cifoot.php
@@ -4,7 +4,7 @@
 
 <footer class="main-footer">
 	<div class="pull-right hidden-xs">Version 0.1.0</div>
-	<strong>Copyright &copy; 2018 <a href="http://geocities.ws/gmt">GMT</a> NK.</strong> All rights reserved.
+	<strong>Copyright &copy; 2019 <a href="<?php echo base_url(); ?>">LTE</a> CI.</strong> All rights reserved.
 </footer>
 
 </div><!-- wrapper -->
