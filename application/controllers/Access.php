a020db29c2a0207af131ec3ef34dc4c7c66ee346
Lupine V <34464643+lupinev@users.noreply.github.com>
2019-04-07 11:20:10 +0800

    Testing Add of TODO

--- application/controllers/Access.php
+++ application/controllers/Access.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Access extends CI_Controller
 {
-
+	// TODO Testing
 	public function index()
 	{
 		$this->load->view('welcome_message');
