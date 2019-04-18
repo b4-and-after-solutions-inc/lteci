b82aef179b1e6186847d7c00a5f8b94fa572b6e9
Felmar <felmar.me@gmail.com>
2019-04-18 09:02:09 +0800

    Database Version 1 revised

--- database/b4_inventory_db.sql
+++ database/b4_inventory_db.sql
@@ -23,9 +23,76 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table `admin`
 --
 
+DROP TABLE IF EXISTS `tbl_admin`;
+CREATE TABLE IF NOT EXISTS `tbl_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `mi` varchar(10) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `contact` varchar(50) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `email_address` varchar(255) DEFAULT NULL,
+  `isactive` bit(1) NOT NULL DEFAULT b'1',
+  `createdby_id` int(11) DEFAULT NULL,
+  `created_datetime` datetime DEFAULT NULL,
+  `updatedby_id` int(11) DEFAULT NULL,
+  `updated_datetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `tbl_business`
+--
+DROP TABLE IF EXISTS `tbl_business`;
+CREATE TABLE IF NOT EXISTS `tbl_business` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `business_name` varchar(255) DEFAULT NULL,
+  `business_logo` varchar(200) DEFAULT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `contact` varchar(50) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `createdby_id` int(11) DEFAULT NULL,
+  `created_datetime` datetime DEFAULT NULL,
+  `updatedby_id` int(11) DEFAULT NULL,
+  `updated_datetime` datetime DEFAULT NULL,
+  `isactive` bit(1) NOT NULL DEFAULT b'1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `tbl_user`
+--
+
+DROP TABLE IF EXISTS `tbl_user`;
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `MI` varchar(10) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `contact` varchar(50) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `email_address` varchar(255) DEFAULT NULL,
+  `isactive` bit(1) NOT NULL DEFAULT b'1',
+  `createdby_id` int(11) DEFAULT NULL,
+  `created_datetime` datetime DEFAULT NULL,
+  `updatedby_id` int(11) DEFAULT NULL,
+  `updated_datetime` datetime DEFAULT NULL,
+  `business_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+--
+-- Table structure for table `orders`
+--
+DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `title` varchar(250) DEFAULT NULL,
@@ -42,7 +109,7 @@ CREATE TABLE `orders` (
 --
 -- Table structure for table `products`
 --
-
+DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `part_number` varchar(250) DEFAULT NULL,
@@ -59,7 +126,7 @@ CREATE TABLE `products` (
 --
 -- Table structure for table `purchases`
 --
-
+DROP TABLE IF EXISTS `purchases`;
 CREATE TABLE `purchases` (
   `id` int(11) NOT NULL,
   `supplier_id` int(11) DEFAULT NULL,
@@ -73,7 +140,7 @@ CREATE TABLE `purchases` (
 --
 -- Table structure for table `suppliers`
 --
-
+DROP TABLE IF EXISTS `suppliers`;
 CREATE TABLE `suppliers` (
   `id` int(11) NOT NULL,
   `supplier_name` varchar(250) DEFAULT NULL
